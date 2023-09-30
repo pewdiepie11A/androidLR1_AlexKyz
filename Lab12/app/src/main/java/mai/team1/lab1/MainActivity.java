ab264c60d895f866cc8ab12d2f9ccb501ca3b009
JolyGot <kuznetsov_akn@mail.ru>
2023-09-30 16:08:46 +0300

    Lab1

--- Lab12/app/src/main/java/mai/team1/lab1/MainActivity.java
+++ Lab12/app/src/main/java/mai/team1/lab1/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+   int y=8;
+
+
 }
